95df34eaf09c541bdcd3cc4f1f92c4a43949ceaf
tech-camp <tech-camp@tech-camp-1-109-no-MacBook-Air.local>
2017-05-26 16:20:11 +0900

    commit

---
+++ resources/views/users/show.blade.php
@@ -0,0 +1,33 @@
+@extends('layout')
+
+@section('content')
+<div class ="contents row">
+  <div class="content_post" style="background-image: url({{ $tweet->image }});">
+    @if (Auth::check())
+      <div class="more">
+        <span><img src="/images/arrow_top.png"></img></span>
+        <ul class="more_list">
+          <li><a href="/tweets/{{ $tweet->id }}">詳細</a></li>
+          <li><a href="/tweets/{{ $tweet->id }}/edit">編集</a></li>
+          <li><a href="/tweets/{{ $tweet->id }}" data-method="delete">削除</a></li>
+        </ul>
+      </div>
+
+      <p>{{$tweet->text}}</p>
+      <span class="name">
+        <a href="#"><span>投稿者</span>{{Auth::user()->name}}</a>
+      </span>
+    @endif
+  </div>
+
+  <div class="container">
+    <!-- ここからフォーム -->
+    @if (Auth::check())
+      {{ Form::open(['url' => "/tweets/{$tweet->id}/comments"]) }}
+        <textarea cols="30" name="text" placeholder="コメントする" rows="2"></textarea>
+        <input type="submit" value="SENT">
+      {{ Form::close() }}
+    @endif
+  </div>
+</div>
+@endsection
